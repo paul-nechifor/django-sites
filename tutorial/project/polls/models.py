0d272c68b9c154149cd29dfa3fc530cfc6ff91a3
Paul Nechifor <paul@nechifor.net>
2014-11-21 07:04:11 +0000

    Fix time bug.

--- tutorial/project/polls/models.py
+++ tutorial/project/polls/models.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import timedelta
 
 from django.db import models
 from django.utils import timezone
@@ -9,7 +9,8 @@ class Question(models.Model):
     pub_date = models.DateTimeField('data published')
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - timedelta(days=-1) <= self.pub_date <= now
 
     def __unicode__(self):
         return self.question_text
