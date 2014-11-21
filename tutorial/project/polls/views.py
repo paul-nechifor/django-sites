1bbe1f49c15263566594b368771af122060f5e0f
Paul Nechifor <paul@nechifor.net>
2014-11-21 05:30:01 +0000

    Add basic views.

--- tutorial/project/polls/views.py
+++ tutorial/project/polls/views.py
@@ -2,4 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse('Polls index.')
+
+def detail(request, question_id):
+    return HttpResponse('Question %s.' % question_id)
+
+def results(request, question_id):
+    return HttpResponse('Results for question: %s.' % question_id)
+
+def vote(request, question_id):
+    return HttpResponse('Vote on question: %s.' % question_id)
