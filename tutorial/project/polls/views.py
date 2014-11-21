384d3e9680d75106d4c131312af87c015bc99ec7
Paul Nechifor <paul@nechifor.net>
2014-11-21 06:50:35 +0000

    Switch to class views.

--- tutorial/project/polls/views.py
+++ tutorial/project/polls/views.py
@@ -1,28 +1,32 @@
 from django.core.urlresolvers import reverse
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from django.views import generic
 
-from .models import Question
-
-
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
+from .models import Choice, Question
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last n published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
 
-def vote(request, question_id):
-    q = get_object_or_404(Question, pk=question_id)
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+
+def vote(request, pk):
+    q = get_object_or_404(Question, pk=pk)
     try:
         selected = q.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
